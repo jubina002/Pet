3421c22337244cabcaa1fe1e40360e29a28fc5a7
leyuskckiran1510 <kdhakal1510@gmail.com>
2024-05-18 14:54:28 +0545

    feat: add some features
    1) add backedn routes for home,sell,adopt, ..
    2) used template block for navbar
    3) made testomoniolas sliding

--- pet.py
+++ pet.py
@@ -1,11 +1,36 @@
 from flask import Flask, render_template
  
 app = Flask(__name__)
- 
+app.config['TEMPLATES_AUTO_RELOAD']=True
+
  
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/adopt")
+def adopt():
+    return render_template("adopt.html")
+
+
+
+
+@app.route("/sell")
+def sell():
+    return render_template("sell.html")
+
+
+
  
 if __name__ == "__main__":
     app.run()
