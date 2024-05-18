aac4f63c3b23cdc994d228d1fe32f4a737aa76ab
jubina002 <jkkhatri013@gmail.com>
2024-05-18 18:13:01 +0545

    Frontend almost complete

--- pet.py
+++ pet.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template
+from flaskext.mysql import MySQL
+
+mysql = MySQL()
  
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD']=True
+mysql.init_app(app)
+cursor = mysql.get_db().cursor()
 
- 
 @app.route("/")
 def index():
     return render_template("index.html")
@@ -22,14 +26,18 @@ def about():
 def adopt():
     return render_template("adopt.html")
 
-
-
-
 @app.route("/sell")
 def sell():
     return render_template("sell.html")
 
+@app.route("/seemore")
+def seemore():
+    return render_template("seemore.html")
 
+
+@app.route("/login")
+def account():
+    return render_template("account.html")
 
  
 if __name__ == "__main__":
